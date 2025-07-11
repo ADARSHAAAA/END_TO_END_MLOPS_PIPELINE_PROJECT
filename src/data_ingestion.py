dc611f1f348dc302661292a884a078db8eb15407
adarsh <adarsha1892004@gmail.com>
2025-07-11 10:35:00 +0530

    experiment tracking using dvc done

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 import logging
+import yaml 
 
 
 # ensure the logs directory exist 
@@ -29,6 +30,25 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 
 
@@ -83,7 +103,12 @@ def main():
 
     try:
 
-        test_size = 0.2
+        # test_size = 0.2
+
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+
+
         data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/spam.csv'
         df = load_data(data_url=data_path)
 
